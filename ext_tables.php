9d4e42e9fe37089ce035a03693da5be74b226f3e
kevin <kevinditscheid@gmail.com>
2015-06-27 23:28:11 +0200

    add table structure for robots meta tag Task #2 - Improve meta data

--- ext_tables.php
+++ ext_tables.php
@@ -13,24 +13,65 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', array(
     'robots_index' => array(
         'exclude' => 1,
-        'label' => 'LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index',
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index',
         'config' => array(
             'type' => 'select',
             'items' => array(
-                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.index','index'),
-                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.noindex','noindex'),
+                array('LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.index','index'),
+                array('LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.noindex','noindex'),
             )
         )
     ),
     'robots_follow' => array(
         'exclude' => 1,
-        'label' => 'LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow',
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow',
         'config' => array(
             'type' => 'select',
             'items' => array(
-                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.follow','follow'),
-                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.nofollow','nofollow'),
+                array('LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.follow','follow'),
+                array('LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.nofollow','nofollow'),
             )
         )
     ),
+    'robots_nosnippet' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_nosnippet',
+        'config' => array(
+            'type' => 'check',
+            'default' => 0,
+        )
+    ),
+    'robots_noodp' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_noodp',
+        'config' => array(
+            'type' => 'check',
+            'default' => 0,
+        )
+    ),
+    'robots_noarchive' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_noarchive',
+        'config' => array(
+            'type' => 'check',
+            'default' => 0,
+        )
+    ),
+    'robots_noimageindex' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_noimageindex',
+        'config' => array(
+            'type' => 'check',
+            'default' => 0,
+        )
+    ),
+    'robots_unavailable_after' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_unavailable_after',
+        'config' => array(
+            'type' => 'check',
+            'default' => 0,
+        )
+    ),
 ));
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('pages', 'metatags', '--linebreak--,robots_index,robots_follow,--linebreak--,robots_nosnippet,robots_noodp,robots_noarchive,robots_noimageindex,robots_unavailable_after', 'after:description');
