6bffbc822dbeed8c225e6a89b2f167cbf8ce2163
Kevin Ditscheid <kevinditscheid@gmail.com>
2015-06-27 17:56:08 +0200

    add collumns to pages table for configuration of robots meta Task #2 - Improve meta data

--- ext_tables.php
+++ ext_tables.php
@@ -9,3 +9,28 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:css_styled_content/static/setup.txt">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kd_base/Configuration/TypoScript/Constants/Constants.ts">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kd_base/Configuration/TypoScript/Setup/Setup.ts">');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', array(
+    'robots_index' => array(
+        'exclude' => 1,
+        'label' => 'LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index',
+        'config' => array(
+            'type' => 'select',
+            'items' => array(
+                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.index','index'),
+                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_index.noindex','noindex'),
+            )
+        )
+    ),
+    'robots_follow' => array(
+        'exclude' => 1,
+        'label' => 'LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow',
+        'config' => array(
+            'type' => 'select',
+            'items' => array(
+                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.follow','follow'),
+                array('LLL:kd_base/Resources/Private/Language/locallang_db.xlf:pages.robots_follow.nofollow','nofollow'),
+            )
+        )
+    ),
+));
