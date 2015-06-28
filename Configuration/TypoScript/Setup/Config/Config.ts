5a500deb0523da82c91d8f4fa2946501cc352c92
kevin <kevinditscheid@gmail.com>
2015-06-28 21:11:54 +0200

    add more debug values and more security configuration Task #7 - Add more debug configuration options

--- Configuration/TypoScript/Setup/Config/Config.ts
+++ Configuration/TypoScript/Setup/Config/Config.ts
@@ -6,26 +6,5 @@
  *
  *    Coded with Netbeans!
  */
-config{
-    prefixLocalAnchors = all
-    baseUrl = {$baseconfig.baseUrl.protocoll}{$baseconfig.baseUrl.server}/
-    absRefPrefix = {$baseconfig.absRefPrefix}
-    adminPanel = 0
-    debug = {$baseconfig.debug}
-
-    simulateStaticDocuments = {$baseconfig.simulateStaticDocuments}
-    tx_realurl_enable = {$baseconfig.tx_realurl_enable}
-}
-[globalVar = LIT:0 = {$baseconfig.baseUrl.enable}]
-config.baseUrl >
-[global]
-[globalVar = LIT:1 = {$baseconfig.tx_realurl_enable}]
-config.simulateStaticDocuments = 0
-[global]
-[globalVar = LIT:1 = {$baseconfig.debug}]
-config{
-    tx_realurl_enable = 0
-    simulateStaticDocuments = 0
-    adminPanel = 1
-}
-[global]
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kd_base/Configuration/TypoScript/Setup/Config/General.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:kd_base/Configuration/TypoScript/Setup/Config/Debug.ts">
